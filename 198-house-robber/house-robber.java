78707a9f61a2a7bf52ae279ef3b9d189ce72dff4
Anurag vivek <153220071+ANURAGVIVEK0919@users.noreply.github.com>
2026-08-24 01:21:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (38.13%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -7,7 +7,7 @@ class Solution {
             return nums[0];
         }
 
-        int[] dp = new int[n];
+        int[] dp = new int[n+1];
 
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
