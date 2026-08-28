24ae63d67a9f637c1a788131fe0e5a1806685247
Anurag vivek <153220071+ANURAGVIVEK0919@users.noreply.github.com>
2026-08-28 23:24:43 +0530

    Time: 1 ms (80.65%) | Memory: 43.2 MB (48.59%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,29 +1,48 @@
 class Solution {
     public int numDecodings(String s) {
+
         int n = s.length();
 
-        if (n == 0 || s.charAt(0) == '0')
+        if (s.charAt(0) == '0') {
             return 0;
-
-        int[] dp = new int[n + 1];
-
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-
-            int oneDigit = s.charAt(i - 1) - '0';
-
-            int twoDigit =
-                Integer.parseInt(s.substring(i - 2, i));
-
-            if (oneDigit >= 1)
-                dp[i] += dp[i - 1];
-
-            if (twoDigit >= 10 && twoDigit <= 26)
-                dp[i] += dp[i - 2];
         }
 
-        return dp[n];
+        int[] dp = new int[n];
+
+        dp[0] = 1;
+
+        if (n >= 2) {
+
+            // i = 1
+            // Single: s[1]
+            // Double: s[0]s[1]
+
+            if (s.charAt(1) != '0') {
+                dp[1] = dp[0];
+            }
+
+            int num = Integer.parseInt(s.substring(0, 2));
+
+            if (num >= 10 && num <= 26) {
+                dp[1] += 1;
+            }
+        }
+
+        for (int i = 2; i < n; i++) {
+
+            // Take current character alone
+            if (s.charAt(i) != '0') {
+                dp[i] += dp[i - 1];
+            }
+
+            // Take current + previous character
+            int num = Integer.parseInt(s.substring(i - 1, i + 1));
+
+            if (num >= 10 && num <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n - 1];
     }
 }
